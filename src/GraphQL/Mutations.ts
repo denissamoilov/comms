90093186f23659646be34273b14309b516f23ff6
Denis Samoilov <denis.samoilov@gmail.com>
2021-08-09 21:55:53 +0300

    Sync with codesandbox

--- src/GraphQL/Mutations.ts
+++ src/GraphQL/Mutations.ts
@@ -1,14 +1,11 @@
 import { gql } from "@apollo/client";
 
 export const ADD_MESSAGE_MUTATION = gql`
-    mutation sendMessage($body: String! $senderName: String!) {
-        insert_messages_one(object: {
-            body: $body,
-            senderName: $senderName
-        }) {
+    mutation sendMessage($body: String!, $senderName: String!) {
+        insert_messages_one(object: { body: $body, senderName: $senderName }) {
             id
             body
             senderName
         }
     }
-`
+`;
